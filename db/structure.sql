d081b83d2c20ab3232358ecfe21ffbff71026348
VitaliiKravchenko <krava18@ukr.net>
2016-09-25 18:01:39 +0300

    add search form and style

--- db/structure.sql
+++ db/structure.sql
@@ -176,6 +176,27 @@ ALTER TABLE ONLY users
 
 
 --
+-- Name: customers_lower_email; Type: INDEX; Schema: public; Owner: -
+--
+
+CREATE INDEX customers_lower_email ON customers USING btree (lower((email)::text));
+
+
+--
+-- Name: customers_lower_first_name; Type: INDEX; Schema: public; Owner: -
+--
+
+CREATE INDEX customers_lower_first_name ON customers USING btree (lower((first_name)::text) varchar_pattern_ops);
+
+
+--
+-- Name: customers_lower_last_name; Type: INDEX; Schema: public; Owner: -
+--
+
+CREATE INDEX customers_lower_last_name ON customers USING btree (lower((last_name)::text) varchar_pattern_ops);
+
+
+--
 -- Name: index_customers_on_email; Type: INDEX; Schema: public; Owner: -
 --
 
@@ -209,6 +230,6 @@ CREATE UNIQUE INDEX index_users_on_reset_password_token ON users USING btree (re
 
 SET search_path TO "$user", public;
 
-INSERT INTO schema_migrations (version) VALUES ('20160925073443'), ('20160925090015'), ('20160925102135');
+INSERT INTO schema_migrations (version) VALUES ('20160925073443'), ('20160925090015'), ('20160925102135'), ('20160925125842');
 
 
