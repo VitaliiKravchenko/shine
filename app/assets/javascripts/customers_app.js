04ca7ad62943a1759f2ed6ec5a92a7abc22fc0df
VitaliiKravchenko <krava18@ukr.net>
2016-10-08 09:27:36 +0300

    create single page app

--- app/assets/javascripts/customers_app.js
+++ app/assets/javascripts/customers_app.js
@@ -1,7 +1,7 @@
 var app = angular.module('customers',['ngRoute', 'templates']);
 app.controller("CustomerSearchController", [
-              "$scope", "$http",
-  function($scope, $http) {
+              "$scope", "$http", "$location",
+  function($scope, $http, $location) {
     var page = 0;
     
     $scope.customers = [];
@@ -15,6 +15,9 @@ app.controller("CustomerSearchController", [
       page = page + 1;
       $scope.search($scope.keywords);
       };
+    $scope.viewDetails = function(customer) {
+      $location.path("/" + customer.id);
+    };
     $scope.search = function(searchTerm) {
       if (searchTerm.length < 3) {
         return;
@@ -38,6 +41,27 @@ app.config([
     $routeProvider.when("/", {
       controller: "CustomerSearchController",
       templateUrl: "customer_search.html"
+    }).when("/:id",{
+      controller: "CustomerDetailController",
+      templateUrl: "customer_detail.html"
     });
   }
 ]);
+
+app.controller("CustomerDetailController", [
+           "$scope", "$http", "$routeParams",
+  function($scope, $http, $routeParams) {
+    var customerId = $routeParams.id;
+    $scope.customer = {};
+
+    $http.get(
+      "/customers/" + customerId + ".json"
+    ).then(function(response) {
+        $scope.customer = response.data;
+      },function(response) {
+        alert("There was a problem: " + response.status);
+      }
+    );
+  }
+]);
+
