1379b69fd2590dfade632bf722d63982b90b830d
VitaliiKravchenko <krava18@ukr.net>
2016-10-09 08:24:19 +0300

    add addresses and billing

--- app/assets/javascripts/customers_app.js
+++ app/assets/javascripts/customers_app.js
@@ -1,67 +1,95 @@
-var app = angular.module('customers',['ngRoute', 'templates']);
-app.controller("CustomerSearchController", [
-              "$scope", "$http", "$location",
-  function($scope, $http, $location) {
-    var page = 0;
-    
-    $scope.customers = [];
-    $scope.previousPage= function() {
-      if (page > 0) {
-        page = page - 1;
-        $scope.search($scope.keywords);
-        }
-      }
-    $scope.nextPage = function() {
-      page = page + 1;
-      $scope.search($scope.keywords);
-      };
-    $scope.viewDetails = function(customer) {
-      $location.path("/" + customer.id);
-    };
-    $scope.search = function(searchTerm) {
-      if (searchTerm.length < 3) {
-        return;
-      };
-      $http.get("/customers.json",
-                { "params": { "keywords": searchTerm, "page": page } }
-      ).then(function(response) {
-        $scope.customers = response.data;
-      },function(response) {
-        alert("There was a problem: " + response.status);
-      }
-    );
-  }
-}
-]);
 
+var app = angular.module(
+  'customers',
+  [
+    'ngRoute',
+    'ngResource',
+    'templates'
+  ]
+); 
 app.config([
-            "$routeProvider",
+          "$routeProvider",
   function($routeProvider) {
-
     $routeProvider.when("/", {
-      controller: "CustomerSearchController",
+       controller: "CustomerSearchController",
       templateUrl: "customer_search.html"
     }).when("/:id",{
-      controller: "CustomerDetailController",
-      templateUrl: "customer_detail.html"
+       controller: "CustomerDetailController",
+      templateUrl: "customer_detail.html",
     });
   }
 ]);
 
-app.controller("CustomerDetailController", [
-           "$scope", "$http", "$routeParams",
-  function($scope, $http, $routeParams) {
-    var customerId = $routeParams.id;
-    $scope.customer = {};
+app.controller("CustomerSearchController", [ 
+          '$scope','$http','$location',
+  function($scope , $http , $location) {                         
 
-    $http.get(
-      "/customers/" + customerId + ".json"
-    ).then(function(response) {
-        $scope.customer = response.data;
-      },function(response) {
-        alert("There was a problem: " + response.status);
+    var page = 0;
+
+    $scope.customers = [];
+    $scope.search = function(searchTerm) {   
+      $scope.loading = true;
+      if (searchTerm.length < 3) {
+        return;
       }
-    );
+      $http.get("/customers.json",  
+                { "params": { "keywords": searchTerm, "page": page } }
+      ).success(
+        function(data,status,headers,config) { 
+          $scope.customers = data;
+          $scope.loading = false;
+      }).error(
+        function(data,status,headers,config) {
+          $scope.loading = false;
+          alert("There was a problem: " + status);
+        });
+    }
+
+    $scope.previousPage = function() {
+      if (page > 0) {
+        page = page - 1;
+        $scope.search($scope.keywords);
+      }
+    }
+    $scope.nextPage = function() {
+      page = page + 1;
+      $scope.search($scope.keywords);
+    }
+
+    $scope.viewDetails = function(customer) {
+      $location.path("/" + customer.id);
+    }
   }
 ]);
 
+app.controller("CustomerDetailController", [ 
+          "$scope","$routeParams","$resource",
+  function($scope , $routeParams , $resource) {
+    $scope.customerId = $routeParams.id;
+    var Customer = $resource('/customers/:customerId.json')
+
+    $scope.customer = Customer.get({ "customerId": $scope.customerId})
+
+    // rest of the controller...
+
+
+    $scope.customer.billingSameAsShipping = false;
+    $scope.$watch('customer.billing_address_id',function() {
+      $scope.customer.billingSameAsShipping = 
+        $scope.customer.billing_address_id == 
+          $scope.customer.shipping_address_id;
+    });
+  }
+]);
+
+app.controller("CustomerCreditCardController", [ 
+          "$scope","$resource",
+  function($scope , $resource) {
+    var CreditCardInfo = $resource('/fake_billing.json')
+    $scope.setCardholderId = function(cardholderId) {
+      $scope.creditCard = CreditCardInfo.get(
+        { "cardholder_id": cardholderId}
+      )
+    }
+  }
+]);
